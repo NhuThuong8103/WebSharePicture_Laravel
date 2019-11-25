b17385ad29d013243fe9c657f3e736497912cd12
Van Sang <sangnguyen07dhth@gmail.com>
2019-11-25 11:29:21 +0700

    lasted sang

--- SharePicture/resources/views/user/home.blade.php
+++ SharePicture/resources/views/user/home.blade.php
@@ -41,9 +41,9 @@
                         <a href="google.com" title="Trang ca nhan">
                             <span class="icon-user">
                                 @if(Auth::check())
-                                    {{ Auth::user()->ho}}
+                                {{ Auth::user()->ho}}
                                 @else
-                                    {{ "" }}
+                                {{ "" }}
                                 @endif
                             </span>
                             <img src="{{ URL::asset('image/icon-user.jpg') }}" class="boder-icon">
@@ -249,7 +249,7 @@
                                                     <div class="modal-dialog" role="document">
                                                         <div class="modal-content">
                                                             <div class="modal-header">
-                                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                 <h2 aria-hidden="true" class="text-primary">&times;</h2>
                                                             </button>
                                                             <h6 class="modal-title" id="exampleModalLabel">New message</h6>
@@ -708,13 +708,7 @@
 
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
-                <script type="text/javascript" src="{{ URL::asset('js/pagination.js') }}"></script>
-                <script type="text/javascript" src="{{ URL::asset('js/main.js') }}"></script>
-                <script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
-
-
             <script type="text/javascript" src="{{ URL::asset('js/pagination.js') }}"></script>
             <script type="text/javascript" src="{{ URL::asset('js/main.js') }}"></script>
             <script>
