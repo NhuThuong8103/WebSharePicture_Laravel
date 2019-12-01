621a7485f4b368aad009e121ddacb3d4bd07b64b
Van Sang <sangnguyen07dhth@gmail.com>
2019-12-01 19:43:19 +0700

    update new photo

--- SharePicture/resources/views/user/newphoto.blade.php
+++ SharePicture/resources/views/user/newphoto.blade.php
@@ -8,59 +8,6 @@
 	<link href="{{ URL::asset('css/pagination.css') }}" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.css">
 	<link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css">
-
-</head>
-<body id="new-photo">
-	@include('user.header')
-	<main>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-2 d-none d-md-block">
-					@include('user.sidebar')
-				</div>
-				<div class="col-lg-8 col-md-12 col-sm-12 col-12 bg-white">
-					<div class="row pt-3">
-						<div class="col-12">
-							<div class="main-title mt-2">
-								<h5>New Photo</h5>
-							</div>
-							<hr class="border-below mt-5">
-						</div>
-					</div>
-					<div class="row pt-2">
-						<div class="col-lg-6">
-							<h6>Title</h6>
-							<input type="text" class="form-control" placeholder="Hôm nay trời đẹp quá hihi" required>
-							<br>
-							<div class="d-lg-none">
-								<h6>Description</h6>
-								<textarea class="form-control" rows="5" placeholder="Bau troi that xanh <3" required></textarea>
-							</div>
-							<br>
-							<h6>Sharing mode</h6>
-							<select name="" id="" class="form-control control-select">
-								<option value="1">Public</option>
-								<option value="2">Private</option>
-							</select>
-							<div class="upload-images">
-								<div class="pic">
-									add image <i class="fas fa-cloud-upload-alt"></i>
-								</div>
-							</div>
-							<div class="group-btn">
-								<a href="{{url('/myphotos/newPhoto/store')}}" class="btn btn-primary">
-									<i class="fas fa-save mr-2"></i>Save
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-6 d-none d-lg-block">
-							<h6>Description</h6>
-							<textarea class="form-control" rows="5" placeholder="Bau troi that xanh <3" required></textarea>
-						</div>
-					</div>
-					<div class="col-lg-2">
-					</div>
-
 @endsection
 
 @section('content')
