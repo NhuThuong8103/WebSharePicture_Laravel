bb45e54b7b3ad4d11dec679fa86d8fb724613c52
Van Sang <sangnguyen07dhth@gmail.com>
2019-12-05 23:01:50 +0700

    sang lasted

--- SharePicture/app/Services/ManagerAlbumService.php
+++ SharePicture/app/Services/ManagerAlbumService.php
@@ -18,10 +18,6 @@ class ManagerAlbumService
 				$arrChiTietAlbumDB=ChitietAlbum::where('album_id',$value['id'])->get();
 
 				$path=GetFileGoogleDriveService::getImagePhoto($value['taikhoan_id'],$value['tieude_album'],$arrChiTietAlbumDB[0]['hinhanh_album']);
-				// foreach ($arrChiTietAlbumDB as $keyCT => $valueCT) {
-				// 	$path=GetFileGoogleDriveService::getImagePhoto($value['taikhoan_id'],$value['tieude_album'],$valueCT['hinhanh_album']);
-				// 	$arrPathCT=array_push($arrPathCT, $path);
-				// }
 
 			 	array_push($array, ['tieude'=>$value['tieude_album'], 'mota' =>$value['mota_album'], 'path'=>$path,'chedo_album'=>$value['chedo_album'],'idalbum' =>$value['id']]);
 		}
